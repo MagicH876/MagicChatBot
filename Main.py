aa60aeae576b0de3bb623dcaca7a906e31eafcbd
MagicH876 <magichopkins188@gmail.com>
2025-02-04 18:18:46 -0500

    Added main bot function (Bot now runs).

--- Main.py
+++ Main.py
@@ -1,7 +1,18 @@
 from dotenv import load_dotenv
-import os
-import discord
+import os, discord
+from discord.ext import commands
 
 load_dotenv()
 
 TOKEN = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged as {bot.user}  :  ID {bot.user.id}\n")
+
+bot.run(TOKEN)
